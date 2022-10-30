0e1d42ef0b85cd1d52185c609f1c8c3cd1471573
DLSYu <105765642+DLSYu@users.noreply.github.com>
2022-10-30 23:13:41 +0800

    Update Tools.java

--- Tools.java
+++ Tools.java
@@ -1,1 +1,19 @@
-toolie
+public class Tools {
+    private String name;
+    private String description;
+    private int amount;
+    private boolean consumable;
+
+    public Tools(String name, String description, int amount, boolean consumable){
+        this.name= name;
+        this.description= description;
+        this.amount = amount;
+        this.consumable= consumable;
+    }
+
+    public void consumeTool(){
+        if(this.consumable && this.amount>0) {
+            this.amount--;
+        }
+    }
+}
